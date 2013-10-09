469a0a0ff69bcc91151125ab62c0e587f5ad4170
Piotr Król <pietrushnic@gmail.com>
2013-10-10 00:09:11 +0200

    new bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+sudo apt-get -y install git vim curl zsh
+curl -L https://get.rvm.io | bash -s stable --ruby
+source ~/.profile
+bash --login
+rvm use ruby
+gem install clenver
+git clone https://github.com/pietrushnic/clenver.git
+git clone https://github.com/pietrushnic/clenver_projects.git projects
