9030bf54972e1b967b2539b0155a685029568999
Piotr Król <pietrushnic@gmail.com>
2013-10-12 23:25:00 +0200

    Call gem and rvm through zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 sudo apt-get -y install git vim curl zsh
 curl -L https://get.rvm.io | bash -s stable --ruby
-source ~/.profile
-rvm use ruby
-gem install clenver
+zsh -l -c 'rvm use ruby'
+zsh -l -c 'gem install clenver'
 git clone https://github.com/pietrushnic/clenver.git
 git clone https://github.com/pietrushnic/clenver_projects.git projects
-zsh
