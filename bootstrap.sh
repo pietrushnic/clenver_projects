3a42b4bfefd1c314342b501dc2cb8ea3333cca9b
Piotr Król <pietrushnic@gmail.com>
2013-10-15 00:54:19 +0200

    remove clenver repo clone and add general project init

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ sudo apt-get -y install git vim curl zsh tmux
 curl -L https://get.rvm.io | bash -s stable --ruby
 zsh -l -c 'rvm use ruby'
 zsh -l -c 'gem install clenver tmuxinator ghi'
-git clone https://github.com/pietrushnic/clenver.git
 git clone https://github.com/pietrushnic/clenver_projects.git projects
+zsh -l -c 'clenver init projects/general.yml'
