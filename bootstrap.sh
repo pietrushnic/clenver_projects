e5ad446055f00ea02eff570ca13c8335f4c04e2e
Piotr Król <pietrushnic@gmail.com>
2013-10-15 00:40:47 +0200

    add tmux, tmuxinator and ghi to autoinstall

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
-sudo apt-get -y install git vim curl zsh
+sudo apt-get -y install git vim curl zsh tmux
 curl -L https://get.rvm.io | bash -s stable --ruby
 zsh -l -c 'rvm use ruby'
-zsh -l -c 'gem install clenver'
+zsh -l -c 'gem install clenver tmuxinator ghi'
 git clone https://github.com/pietrushnic/clenver.git
 git clone https://github.com/pietrushnic/clenver_projects.git projects
