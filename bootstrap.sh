28ec8e0ef7dadcd20ac5e885020f25010dd273d8
Piotr Król <pietrushnic@gmail.com>
2013-10-15 21:55:36 +0200

    initialize general project in src driectory

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 zsh -l -c 'rvm use ruby'
 zsh -l -c 'gem install clenver tmuxinator ghi'
 git clone https://github.com/pietrushnic/clenver_projects.git projects
-zsh -l -c 'clenver init projects/general.yml'
+zsh -l -c 'clenver init projects/general.yml $HOME/src'
