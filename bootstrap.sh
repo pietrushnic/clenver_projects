94120ce9f7f7db054b0909c5f2d4546d6a5bcb91
Piotr Król <pietrushnic@gmail.com>
2013-10-30 15:08:46 +0100

    source rvm script before running rvm or gem

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install git vim curl zsh tmux
 curl -L https://get.rvm.io | bash -s stable --ruby
-zsh -l -c 'rvm use ruby'
-zsh -l -c 'gem install clenver tmuxinator ghi'
+zsh -l -c 'source $HOME/.rvm/scripts/rvm;rvm use ruby'
+zsh -l -c 'source $HOME/.rvm/scripts/rvm;gem install clenver tmuxinator ghi'
 git clone https://github.com/pietrushnic/clenver_projects.git projects
 zsh -l -c 'clenver init projects/general.yml $HOME/src'
