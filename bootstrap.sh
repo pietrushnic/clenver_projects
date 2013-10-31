58cef5684ee4fa38991750cef7f85bf1780228e8
Piotr Król <pietrushnic@gmail.com>
2013-10-31 18:53:49 +0100

    change directory structure

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,5 @@ sudo apt-get -y install git vim curl zsh tmux
 curl -L https://get.rvm.io | bash -s stable --ruby
 zsh -l -c 'source $HOME/.rvm/scripts/rvm;rvm use ruby'
 zsh -l -c 'source $HOME/.rvm/scripts/rvm;gem install clenver tmuxinator ghi'
-git clone https://github.com/pietrushnic/clenver_projects.git projects
-zsh -l -c 'source $HOME/.rvm/scripts/rvm;clenver init projects/general.yml $HOME/src'
+git clone https://github.com/pietrushnic/clenver_projects.git src/
+zsh -l -c 'source $HOME/.rvm/scripts/rvm;clenver init src/clenver_projects/general.yml $HOME/src'
